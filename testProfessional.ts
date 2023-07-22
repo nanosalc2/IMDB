30f9dc3669488251428ee2a54526ddd1aa0c025a
nanosalc2 <nano011081@gmail.com>
2023-07-22 18:54:52 +0200

    atualizar Professional

---
+++ testProfessional.ts
@@ -0,0 +1,7 @@
+import { Professional } from "./Professional";
+
+let rebert = new Professional("Robert D'Niro", 70, 80, 1.75, false, "USA", 3, "actor");
+let keanu = new Professional("Keanu Reeves", 58, 79, 1.86, false, "Canadian", 0, "Actor");
+
+rebert.printAll();
+keanu.printAll();
